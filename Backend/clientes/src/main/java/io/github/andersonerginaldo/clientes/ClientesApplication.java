861eca5561ecb36ebb4ae6fea0d8db2e733a2114
Anderson Reginaldo <anderson.reginaldo@gmail.com>
2022-09-01 17:32:28 -0300

    Criação da API com SpringBoot

--- Backend/clientes/src/main/java/io/github/andersonerginaldo/clientes/ClientesApplication.java
+++ Backend/clientes/src/main/java/io/github/andersonerginaldo/clientes/ClientesApplication.java
@@ -1,10 +1,25 @@
 package io.github.andersonerginaldo.clientes;
 
+import io.github.andersonerginaldo.clientes.model.entity.Cliente;
+import io.github.andersonerginaldo.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repository){
+		return args ->{
+			Cliente cliente = Cliente.builder().cpf("97511404391").nome("Fulano").build();
+			repository.save(cliente);
+			cliente = Cliente.builder().cpf("65082869353").nome("Beltrano").build();
+			repository.save(cliente);
+		};
+	}
 
 	public static void main(String[] args) {
 
